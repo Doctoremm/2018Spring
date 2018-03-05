d5730077980977144899adb9156b38a6e9469ce9
Doctoremm <doctoremm@gmail.com>
2018-03-05 16:38:02 -0700

    StateMachines, ScriptableObjects, VFX

---
+++ Unity/Testing_Assignments/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public ScriptableObjects player;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(player.health);
+	}
+}
---
+++ Unity/Testing_Assignments/Assets/Scripts/ScriptableObjects.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player")]
+public class ScriptableObjects : ScriptableObject {
+
+	public string playerName;
+
+	public int health;
+	public int attackDamage;
+	public int knockback;
+	
+}
